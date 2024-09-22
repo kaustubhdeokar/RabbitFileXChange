9497941dc9707795bf2e2d83a2a86a461577f827
kaustubhdeokar <kaustubhd9@gmail.com>
2024-09-22 21:37:15 +0530

    rabbit mq producer consumer scenario, file exchange between two hosts

---
+++ consumer/consumer.js
@@ -0,0 +1,44 @@
+const amqp = require('amqplib/callback_api');
+const fs = require('fs');
+const path = require('path');
+
+amqp.connect('amqp://localhost', (error0, connection) => {
+    if (error0) throw error0;
+
+    connection.createChannel((error1, channel) => {
+        if (error1) throw error1;
+
+        const queue = 'file_updates';
+        const consumerDir = path.join(__dirname, '/../consumer_directory');
+
+        if (!fs.existsSync(consumerDir)) {
+            fs.mkdirSync(consumerDir);
+        }
+
+        channel.checkQueue(queue, (err, ok) => {
+            if (err) {
+                console.log('Queue does not exist.');
+                return;
+            }
+            console.log(`Messages in queue: ${ok.messageCount}`);
+        });
+
+        console.log(`Waiting for messages in ${queue}. To exit press CTRL+C`);
+
+        channel.consume(queue, (msg) => {
+            if (msg !== null) {
+                const content = JSON.parse(msg.content.toString());
+                const fileName = content.fileName;
+                const fileContent = Buffer.from(content.fileContent, 'base64');
+                const destPath = path.join(consumerDir, fileName);
+
+                fs.writeFile(destPath, fileContent, (err) => {
+                    if (err) throw err;
+                    console.log(`[Consumer] File received and saved as: ${destPath}`);
+                    // Acknowledge the message
+                    channel.ack(msg);
+                });
+            }
+        });
+    });
+});
---
+++ producer/main.js
@@ -0,0 +1,67 @@
+const amqp = require('amqplib/callback_api');
+const fs = require('fs');
+const path = require('path');
+
+function queueFile(filePath) {
+    amqp.connect('amqp://localhost', (error0, connection) => {
+        if (error0) throw error0;
+
+        connection.createChannel((error1, channel) => {
+            if (error1) throw error1;
+
+            const queue = 'file_updates';
+            fs.readFile(filePath, (err, data) => {
+                if (err) throw err;
+
+                const message = {
+                    fileName: path.basename(filePath),
+                    fileContent: data.toString('base64')
+                };
+                channel.assertQueue(queue, { durable: true });
+                channel.sendToQueue(queue, Buffer.from(JSON.stringify(message)));
+                console.log(`[Producer] Sent file: ${filePath}`);
+                setTimeout(() => {
+                    connection.close();
+                    process.exit(0);
+                }, 500);
+            });
+        });
+    });
+}
+
+async function chooseDirectory() {
+
+    const path = require('path');
+    const prompt = require('prompt-sync')({ sigint: true });
+
+    const defaultDirectory = '../directory';
+    const fullPath = path.resolve(defaultDirectory);
+    const directory = prompt('Select a directory to copy files to the client. The default directory is ../directory.') || fullPath;
+    console.log(`Copying files from ${directory}`);
+
+    return directory;
+}
+
+(async function main() {
+
+    try {
+        const dirPath = await chooseDirectory();
+
+        const files = fs.readdirSync(dirPath);
+
+
+        files.forEach(file => {
+            const filePath = path.join(dirPath, file);
+            if (fs.lstatSync(filePath).isFile()) {
+                queueFile(filePath);
+            }
+        });
+
+    }
+    catch (err) {
+        console.error('Error:', err);
+    }
+})
+    ();
+
+
